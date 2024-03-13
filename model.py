4793b57fd2362b8cd4252b4b80b358c29fc72a0a
milopanchal004 <158142694+milopanchal004@users.noreply.github.com>
2024-03-13 12:14:08 +0530

    Update model.py

--- model.py
+++ model.py
@@ -2,25 +2,68 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+
+
 # Functions and procedures
-def plot_predictions(train_data, train_labels, test_data, test_labels, predictions):
-"""
-Plots training data, test data and compares predictions.
-"""
-plt.figure(figsize=(6, 5))
-# Plot training data in blue
-plt.scatter(train_data, train_labels, c="b", label="Training data")
-# Plot test data in green
-plt.scatter(test_data, test_labels, c="g", label="Testing data")
-# Plot the predictions in red (predictions were made on the test data)
-plt.scatter(test_data, predictions, c="r", label="Predictions")
-# Show the legend
-plt.legend(shadow='True')
-# Set grids
-plt.grid(which='major', c='#cccccc', linestyle='--', alpha=0.5)
-# Some text
-plt.title('Model Results', family='Arial', fontsize=14)
-plt.xlabel('X axis values', family='Arial', fontsize=11)
-plt.ylabel('Y axis values', family='Arial', fontsize=11)
-# Show
-plt.savefig('model_results.png', dpi=120)
+def plot_predictions(train_data, train_labels,  test_data, test_labels,  predictions):
+  """
+  Plots training data, test data and compares predictions.
+  """
+  plt.figure(figsize=(6, 5))
+  # Plot training data in blue
+  plt.scatter(train_data, train_labels, c="b", label="Training data")
+  # Plot test data in green
+  plt.scatter(test_data, test_labels, c="g", label="Testing data")
+  # Plot the predictions in red (predictions were made on the test data)
+  plt.scatter(test_data, predictions, c="r", label="Predictions")
+  # Show the legend
+  plt.legend(shadow='True')
+  # Set grids
+  plt.grid(which='major', c='#cccccc', linestyle='--', alpha=0.5)
+  # Some text
+  plt.title('Model Results', family='Arial', fontsize=14)
+  plt.xlabel('X axis values', family='Arial', fontsize=11)
+  plt.ylabel('Y axis values', family='Arial', fontsize=11)
+  # Show
+  plt.savefig('model_results.png', dpi=120)
+
+
+
+def mae(y_test, y_pred):
+  """
+  Calculuates mean absolute error between y_test and y_preds.
+  """
+  return tf.metrics.mean_absolute_error(y_test, y_pred)
+  
+
+def mse(y_test, y_pred):
+  """
+  Calculates mean squared error between y_test and y_preds.
+  """
+  return tf.metrics.mean_squared_error(y_test, y_pred)
+
+
+# Check Tensorflow version
+print(tf.__version__)
+
+
+# Create features
+X = np.arange(-100, 100, 4)
+
+# Create labels
+y = np.arange(-90, 110, 4)
+
+
+# Split data into train and test sets
+X_train = X[:40] # first 40 examples (80% of data)
+y_train = y[:40]
+
+X_test = X[40:] # last 10 examples (20% of data)
+y_test = y[40:]
+
+
+# Take a single example of X
+input_shape = X[0].shape 
+
+# Take a single example of y
+output_shape = y[0].shape
